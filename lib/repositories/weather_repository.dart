a179a8c445f09bf1459d86ed2d6a538289cbce12
alexg <alex.gorbanev05@gmail.com>
2024-12-23 20:34:37 +0500

    refactor: made a weather repo constructor constant

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -3,6 +3,8 @@ import 'package:flutter_weather_app/models/coordinates.dart';
 import 'package:flutter_weather_app/models/weather.dart';
 
 abstract class WeatherRepository {
+  const WeatherRepository();
+
   Future<Either<String, Weather>> getWeatherToday(String city);
   Future<Either<String, List<Weather>>> getWeatherNdaysAhead(int nDays, String city);
   Future<Either<String, Coordinates>> getCoordinatesFromCityName(String city);
