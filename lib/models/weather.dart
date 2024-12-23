0bac799a351585bc49d145f95e07ad9ecc251998
alexg <alex.gorbanev05@gmail.com>
2024-12-23 18:44:28 +0500

    feat: added new fields to the weather model

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -2,6 +2,9 @@ final class Weather {
   final double temperature;
   final double feelsLike;
   final double windSpeed;
+  final int humidity;
+  final int pressure;
+  final String weatherDescription;
 
-  Weather({required this.temperature, required this.feelsLike, required this.windSpeed});
+  const Weather({required this.temperature, required this.feelsLike, required this.windSpeed, required this.humidity, required this.pressure, required this.weatherDescription});
 }
