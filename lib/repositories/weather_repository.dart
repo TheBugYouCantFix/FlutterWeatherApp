955f28cb6aea300bf1cbe5fa1fc5d9fff07b23d5
alexg <alex.gorbanev05@gmail.com>
2024-12-23 17:53:59 +0500

    refactor: weather repository methods return either monads

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -1,8 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_weather_app/models/coordinates.dart';
 import 'package:flutter_weather_app/models/weather.dart';
 
 abstract class WeatherRepository {
-  Future<Weather> getWeatherToday(String city);
-  Future<List<Weather>> getWeatherNdaysAhead(String city);
-  Future<Coordinates> getCoordinatesFromCityName(String city);
+  Future<Either<String, Weather>> getWeatherToday(String city);
+  Future<Either<String, List<Weather>>> getWeatherNdaysAhead(String city);
+  Future<Either<String, Coordinates>> getCoordinatesFromCityName(String city);
 }
