44386781d7981c2239a5d37bd81c924570b477d5
alexg <alex.gorbanev05@gmail.com>
2024-12-23 21:25:56 +0500

    feat: new state added to the weather state

--- lib/cubit/weather_state.dart
+++ lib/cubit/weather_state.dart
@@ -13,25 +13,29 @@ final class WeatherState extends Equatable {
   final WeatherStatus status;
   final Weather? weather;
   final List<Weather>? nDaysAheadWeather;
+  final String? errorMessage;
 
   const WeatherState({
     this.status = WeatherStatus.initial,
     this.weather,
     this.nDaysAheadWeather = const [],
+    this.errorMessage
   });
 
   WeatherState copyWith({
     WeatherStatus? status,
     Weather? weather,
     List<Weather>? nDaysAheadWeather,
+    String? errorMessage
   }) {
     return WeatherState(
       status: status ?? this.status,
       weather: weather ?? this.weather,
       nDaysAheadWeather: nDaysAheadWeather ?? this.nDaysAheadWeather,
+      errorMessage: errorMessage ?? this.errorMessage
     );
   }
 
   @override
-  List<Object?> get props => [status, weather, nDaysAheadWeather];
+  List<Object?> get props => [status, weather, nDaysAheadWeather, errorMessage];
 }
