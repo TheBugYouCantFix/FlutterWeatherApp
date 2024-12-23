cf3a067f36de78509b29ce4d13a66a59a1e6cfb4
alexg <alex.gorbanev05@gmail.com>
2024-12-23 19:15:55 +0500

    fix: added a must-have method argument

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -5,5 +5,5 @@ import 'package:flutter_weather_app/models/weather.dart';
 abstract class WeatherRepository {
   Future<Either<String, Weather>> getWeatherToday(String city);
   Future<Either<String, List<Weather>>> getWeatherNdaysAhead(String city);
-  Future<Either<String, Coordinates>> getCoordinatesFromCityName(String city);
+  Future<Either<String, Coordinates>> getCoordinatesFromCityName(int nDays, String city);
 }
