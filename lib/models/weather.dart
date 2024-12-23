d693bf8c7c1697695030932d3500f987517641ff
alexg <alex.gorbanev05@gmail.com>
2024-12-23 19:16:29 +0500

    feat: added a new weather factort constructor from json

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 final class Weather {
   final double temperature;
   final double feelsLike;
@@ -7,4 +9,15 @@ final class Weather {
   final String weatherDescription;
 
   const Weather({required this.temperature, required this.feelsLike, required this.windSpeed, required this.humidity, required this.pressure, required this.weatherDescription});
+  factory Weather.fromOpenWeatherApiJson(String jsonData) {
+    Map<String, dynamic> data = jsonDecode(jsonData);
+    return Weather(
+      temperature: data['main']['temp'],
+      feelsLike: data['main']['feels_like'],
+      windSpeed: data['wind']['speed'],
+      humidity: data['main']['humidity'],
+      pressure: data['main']['pressure'],
+      weatherDescription: data['weather'][0]['description'],
+    );
+  }
 }
