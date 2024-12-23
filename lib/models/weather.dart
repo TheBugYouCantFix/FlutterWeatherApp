84b07a17dbf42785d3f3b2457f40789a6f35fa5f
alexg <alex.gorbanev05@gmail.com>
2024-12-23 17:31:15 +0500

    feat: weather model

---
+++ lib/models/weather.dart
@@ -0,0 +1,7 @@
+final class Weather {
+  final double temperature;
+  final double feelsLike;
+  final double windSpeed;
+
+  Weather({required this.temperature, required this.feelsLike, required this.windSpeed});
+}
